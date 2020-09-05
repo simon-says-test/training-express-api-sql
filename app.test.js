63842d8be8c0d755004b4b1769e091e58b98ccd9
Ubuntu <simon@graduate-induction-simon.l2erzjxdnnbu3c1mnssa2s2flb.fx.internal.cloudapp.net>
2020-09-05 08:40:38 +0000

    fix test

--- app.test.js
+++ app.test.js
@@ -1,22 +1,50 @@
+require('dotenv').config();
 const request = require("request-promise-native");
+const { Connection } = require('./mongo-connection');
+let client;
+let people;
+
+const establishConnection = async (collection) => {
+  client = await Connection.connectToMongo();
+  return client.db('training-simon').collection(collection);
+}
 
 const baseUrl = "http://localhost:3000/people";
 
-describe("PUT to /people/:id", () => {
-  describe("Given a valid person", () => {
-    let response;
-    beforeEach(async () => {
-      const requestOptions = {
-          uri: `${baseUrl}/1`,
-          json: true,
-          method: "PUT",
-          body: { id: 2, firstName: "Simon", lastName: "Thomas", age: 39 }
-        };
-        response = await request(requestOptions);
-    });
+const resetDb = async (people) => {
+  await people.deleteMany({});
+  await people.insertMany([
+    { id: 1, firstName: "Bob", lastName: "Bobbs", age: 39 },
+    { id: 2, firstName: "Tee", lastName: "Bone", age: 45 }
+  ]);
+};
 
-    it("should return a success message", () => {
-      expect(response).toBe("Update successful");
+describe("POST to /people", () => {
+  beforeAll(async () => {
+    people = await establishConnection('people');
+  })
+  
+  beforeEach(async function() {
+    await resetDb(people);
+  });
+
+  describe("Given a valid person", () => {
+    const requestOptions = {
+      uri: `${baseUrl}`,
+      json: true,
+      method: "POST",
+      body: { firstName: "Simon", lastName: "Thomas", age: 39 }
+    };
+    it("should return a success status and message", async () => {
+      const response = await request(requestOptions);
+      console.log(response);
+      //expect(response.statusCode).toBe(200);
+      expect(response).toBe("Creation successful");
     });
   });
+
+  afterAll(async () => {
+    people = null;
+    await client.close();
+  })
 });  
